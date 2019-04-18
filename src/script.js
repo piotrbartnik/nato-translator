92f7226d288444c86bc8d6ffc0b9a15b6f11ee42
piotrbartnik <piotr.bartnik@hotmail.com>
2019-04-18 21:42:12 +0200

    initial

---
+++ src/script.js
@@ -0,0 +1,31 @@
+var synth = window.speechSynthesis;
+
+var inputForm = document.querySelector('form');
+var inputTxt = document.querySelector('.txt');
+var voiceSelect = document.querySelector('select');
+
+
+
+function speak(){
+    if (synth.speaking) {
+        console.error('speechSynthesis.speaking');
+        return;
+    }
+    if (inputTxt.value !== '') {
+    var utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+    utterThis.onend = function (event) {
+        console.log('SpeechSynthesisUtterance.onend');
+    }
+    utterThis.onerror = function (event) {
+        console.error('SpeechSynthesisUtterance.onerror');
+    }
+    synth.speak(utterThis);
+  }
+}
+
+inputForm.onsubmit = function(event) {
+  event.preventDefault();
+  speak();
+  inputTxt.blur();
+}
+
